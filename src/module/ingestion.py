de164652764b2faccf3515ae22a18d68e2245f22
Alessandro Silva <alessandro-psilva@outlook.com>
2023-12-02 20:14:18 -0300

    refactor: factory -> model

--- src/module/ingestion.py
+++ src/module/ingestion.py
@@ -5,7 +5,7 @@ class Ingestion:
     def process(self, payload: Dict) -> Dict:
         self._payload: Dict = payload
         self._payload["cdIngestao"] = int(uuid.uuid4())
-        self._factory: Union[Avro, Bin, Csv, Json, Orc, Parquet, Xlsx, Kafka] = eval(
+        self._model: Union[Avro, Bin, Csv, Json, Orc, Parquet, Xlsx, Kafka] = eval(
             payload["cdTipoIngestao"]
         )
-        self._factory(payload=self._payload).process_data()
+        self._model(payload=self._payload).process_data()
