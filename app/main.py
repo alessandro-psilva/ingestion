f80337639e39c3c6e8f3dff04397e06d385d18d8
Alessandro Silva <alessandro-psilva@outlook.com>
2024-02-18 15:54:22 +0000

    refactor: PyLint

--- app/main.py
+++ app/main.py
@@ -1,13 +1,16 @@
+"""Module main to ingestion file."""
+
 import os
 import json
 from module.ingestion import Ingestion
 
 
 def main():
+    """Function main()"""
     path_app = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-    file_path = os.path.join(path_app, "ingestion", "app", "data", "payloads.json")    
+    file_path = os.path.join(path_app, "ingestion", "app", "data", "payloads.json")
 
-    with open(file_path, "r") as file:
+    with open(file=file_path, encoding="utf-8", mode="r") as file:
         payloads = json.load(file)
 
     ingestion = Ingestion()
